4476d50d5494f92f2dbdd96b0b80e5c775ade128
kuro <93817520+EIJIKUROKAWA@users.noreply.github.com>
2022-01-25 12:46:49 +0900

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <form class="ui form">
+  <div class="field">
+    <label>First Name</label>
+    <input type="text" name="first-name" placeholder="First Name" />
+  </div>
+
+</form>
+  );
+};
+
+export default SearchBar;
